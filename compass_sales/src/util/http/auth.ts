3355addf770881f01be408f6736126a13ae5bb8c
Gerdany Junior <Asa_lindo@hotmail.com>
2023-09-15 15:20:41 -0300

    feat: :sparkles: create signUp firebase method

---
+++ compass_sales/src/util/http/auth.ts
@@ -0,0 +1,15 @@
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { app } from './firebaseConfig';
+import { Alert } from "react-native";
+
+const auth = getAuth(app);
+
+export async function signUp(email: string, password: string, displayName: string) {
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        await updateProfile(userCredential.user, { displayName: displayName });
+        return userCredential.user;
+    } catch (error: any) {
+        Alert.alert(`${error.code})}`);
+    }
+}
