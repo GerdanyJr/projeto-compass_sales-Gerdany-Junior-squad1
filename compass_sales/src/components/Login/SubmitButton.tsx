b060db650bcd86eb05b8db54f25f1498b692f58a
Gerdany Junior <Asa_lindo@hotmail.com>
2023-09-17 22:48:30 -0300

    feat: :sparkles: add loading state to submit button

--- compass_sales/src/components/Login/SubmitButton.tsx
+++ compass_sales/src/components/Login/SubmitButton.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-import { Pressable, StyleSheet, Text } from "react-native";
+import { ActivityIndicator, Pressable, StyleSheet, Text } from "react-native";
 import { Colors } from "../../util/Colors";
 
 interface SubmitButton {
     title: string,
     onPress?: (...args: any) => void,
+    isLoading?: boolean,
     style?: any
 }
 
@@ -14,7 +15,10 @@ export function SubmitButton(props: SubmitButton): JSX.Element {
         <Pressable style={[styles.buttonContainer, props.style]}
             onPress={props.onPress}
             android_ripple={{ color: '#C62000', foreground: true }}>
-            <Text style={styles.buttonText}>{props.title}</Text>
+            {props.isLoading ?
+                <ActivityIndicator color='white'  /> :
+                <Text style={styles.buttonText}>{props.title}</Text>
+            }
         </Pressable>
     )
 }
